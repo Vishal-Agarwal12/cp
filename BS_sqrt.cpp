46a8c6d1058ae9c389a77f76f279c748a4b22315
Vishal Agarwal <83177871+Vishal-Agarwal12@users.noreply.github.com>
2023-05-18 21:34:30 +0530

    Create BS_sqrt.cpp

---
+++ BS_sqrt.cpp
@@ -0,0 +1,10 @@
+ll bs_sqrt(ll x) {
+  ll left = 0, right = 2000000123;
+  while (right > left) {
+      ll mid = (left + right) / 2;
+      
+      if (mid * mid > x) right = mid;
+      else left = mid + 1;
+  }
+  return left - 1;
+}
