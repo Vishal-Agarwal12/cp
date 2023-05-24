a2406abbb657622f559ca5df0d41cde1aeb0646a
Vishal Agarwal <83177871+Vishal-Agarwal12@users.noreply.github.com>
2023-05-24 12:21:20 +0530

    Create Large value of 2^n.cpp

---
+++ Large value of 2^n.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+void solve()
+{
+    // using ll is necessary otherwise it gives wrong answer
+    int n = 100;
+    cout << (1ll << n) << endl;  // 2^n
+}
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+        solve();
+
+    return 0;
+}
