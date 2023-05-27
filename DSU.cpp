50d627f990a846599e58d24b9da460e85f5277a5
Vishal Agarwal <83177871+Vishal-Agarwal12@users.noreply.github.com>
2023-05-27 09:33:13 +0530

    Create DSU.cpp

---
+++ DSU.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct DSU
+{
+    vector<int> father;
+    vector<int> size;
+    int components;
+    int maxComponentSize;
+
+    DSU(int N)
+    {
+        father = vector<int>(N);
+        size = vector<int>(N, 1);
+        components = N;
+        maxComponentSize = 1;
+        for (int i = 0; i < N; i++)
+        {
+            father[i] = i;
+        }
+    }
+
+    int find(int node)
+    {
+        if (father[node] != node)
+        {
+            father[node] = find(father[node]);
+        }
+        return father[node];
+    }
+
+    void unite(int A, int B)
+    {
+        int rootA = find(A);
+        int rootB = find(B);
+        if (rootA != rootB)
+        {
+            if (size[rootA] < size[rootB])
+            {
+                swap(rootA, rootB);
+            }
+            father[rootB] = rootA;
+            size[rootA] += size[rootB];
+            maxComponentSize = max(maxComponentSize, size[rootA]);
+            components--;
+        }
+    }
+
+    int getMaxComponentSize()
+    {
+        return maxComponentSize;
+    }
+
+    int countComponents()
+    {
+        return components;
+    }
+};
+
+int main()
+{
+    int N, M;
+    cin >> N >> M;
+
+    DSU dsu(N);
+
+    for (int i = 0; i < M ; i++)
+    {
+        int A, B;
+        cin >> A >> B;
+        dsu.unite(A, B);
+        int numComponents = dsu.countComponents();
+        int largestComponentSize = dsu.getMaxComponentSize();
+        cout << numComponents << " " << largestComponentSize << endl;
+    }
+
+    return 0;
+}
+
+// We can also find the size of component containing node
+    int Q;
+    cin >> Q;
+    for (int i = 0; i < Q; i++) {
+        int node;
+        cin >> node;
+        int componentSize = dsu.getComponentSize(node);
+        cout << "Size of component containing node " << node << ": " << componentSize << endl;
+    }
