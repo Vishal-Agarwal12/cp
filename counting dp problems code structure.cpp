06e91bc1e20dfd11a8b418d163561a767504efe3
Vishal Agarwal <83177871+Vishal-Agarwal12@users.noreply.github.com>
2023-09-04 16:49:06 +0530

    Create counting dp problems code structure.cpp

---
+++ counting dp problems code structure.cpp
@@ -0,0 +1,13 @@
+
+
+  f() {
+
+      base case
+      return 1 // if condition satifies
+      return 0 // if not satisfies
+
+      left = f();
+      right = f();
+      
+      return left+right;
+  }
