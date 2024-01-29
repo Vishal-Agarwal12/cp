72c7c1ecbff4460826eea433f9748f64747ac257
Vishal Agarwal <83177871+Vishal-Agarwal12@users.noreply.github.com>
2024-01-29 10:33:45 +0530

    Create 13. Building Roads(from CSES).cpp

---
+++ DSU/13. Building Roads(from CSES).cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#endif
+
+using ll = long long;
+using maxheap = priority_queue<ll>; // maxheap
+using minheap = priority_queue<ll, vector<ll>, greater<ll>>; // minheap
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define sort_and_unique(a) \
+    sort(all(a));          \
+    (a).resize(unique(all(a)) - (a).begin())
+#define minv(a) *min_element(all(a))
+#define maxv(a) *max_element(all(a))
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define nl '\n'
+#define se second
+#define ins insert
+#define sz(x) ((int)(x).size())
+#define pw(x) (1LL << (x))
+#define setbits(x) __builtin_popcountll(x)
+const ll Mod = 998244353;
+const ll mod = 1e9 + 7;
+const ll inf = 1e18;
+
+void solve()
+{
+    ll n, m;
+    cin>>n>>m;
+
+    vector<ll> par(n+1);
+    for(int i=1;i<=n;i++) par[i] = i;
+
+    auto find = [&](auto &&self, ll v) -> ll {
+        if(v == par[v]) return v;
+        return par[v] = self(self, par[v]);
+    };
+
+    auto unite = [&](ll u, ll v) -> void {
+        ll a = find(find, u);
+        ll b = find(find, v);
+
+        if(a != b) {
+            par[b] = a;
+        }
+    };
+
+    while(m--) {
+        ll x, y;
+        cin>>x>>y;
+
+        unite(x, y);
+    }
+
+    vector<pair<ll, ll>> v;
+    for(int i=2;i<=n;i++) {
+        ll a = find(find, 1);
+        ll b = find(find, i);
+
+        if(a != b) {
+            v.pb({1, i});
+            unite(1, i);
+        }
+    }
+
+    cout<<v.size()<<nl;
+    for(auto [x,y]: v) cout<<x<<" "<<y<<nl;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    //freopen("input.txt", "rt", stdin);
+    //freopen("output.txt", "wt", stdout);
+
+    solve();
+
+    return 0;
+}
