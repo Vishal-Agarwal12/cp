1e1e4b86c083261c67a6dbb1616b81d913f196dd
Vishal Agarwal <83177871+Vishal-Agarwal12@users.noreply.github.com>
2024-03-31 08:55:45 +0530

    Create begin-template.cpp

---
+++ begin-template.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#endif
+
+using ll = long long;
+using maxheap = priority_queue<ll>; // maxheap
+using minheap = priority_queue<ll, vector<ll>, greater<ll>>; // minheap
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define sort_and_unique(a) \
+    sort(all(a));          \
+    (a).resize(unique(all(a)) - (a).begin())
+#define minv(a) *min_element(all(a))
+#define maxv(a) *max_element(all(a))
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define nl '\n'
+#define se second
+#define ins insert
+#define sz(x) ((int)(x).size())
+#define pw(x) (1LL << (x))
+#define setbits(x) __builtin_popcountll(x)
+const ll Mod = 998244353;
+const ll mod = 1e9 + 7;
+const ll inf = 1e18;
+
+void solve()
+{
+    ll n;
+    cin>>n;
+    ll a[n];
+    for(int i=0;i<n;i++) cin>>a[i];
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    //freopen("input.txt", "rt", stdin);
+    //freopen("output.txt", "wt", stdout);
+
+    int T;
+    cin>>T;
+    while(T--)
+        solve();
+
+    return 0;
+}
